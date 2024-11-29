d2e0a38df1176c2cccfe3fd633dcbbaebe8b82e7
PrimeEmre <136666281+PrimeEmre@users.noreply.github.com>
2024-11-29 12:04:29 -0500

    added show password and hide password and finished the password entry for my survey .

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def Send():
     else:
         messagebox.showerror(message="You did not answer the survey!")
 
+    # Password
     try:
         user_input = password_entry.get()
         # Try to convert the input to a number (float or int)
@@ -29,6 +30,20 @@ def Send():
     except ValueError:
         # If the input is str, show an error message
         messagebox.showerror(message="You did not write a number for the password!. :(")
+
+
+# Setting the show password
+def show_password():
+    # This if statement is used for hiding password
+    if password_entry.cget('show') == '*':  # get is used for gettign the value of password entry
+        password_entry.config(show='')
+        # Putting text on hide_button
+        hide_button.config(text='Hide Password')
+
+    # Now we are doing else statement for showing the password
+    else:
+        password_entry.config(show='*')
+        hide_button.config(text="Show password")
 
 
 # Setting the title
@@ -51,10 +66,13 @@ last_name_entry.pack()
 # Setting the label and the entry for the Password
 password_label = tkinter.Label(font=("Playwrite England SemiJoined", 12), text="Password", bg="black", fg="white")
 password_label.pack(pady=5)
-
-password_entry = tkinter.Entry(font=("PT Sans", 12), bg="gold", fg="black")
+# created hide password
+password_entry = tkinter.Entry(window, show='*', font=("PT Sans", 12), bg="gold", fg="black")
 password_entry.pack(pady=5)
 
+# creating a button for password visibility
+hide_button = tkinter.Button(window, text="show password", command=show_password)
+hide_button.pack(pady=3)
 # Setting up the send button
 send_button = tkinter.Button(font=("Bona Nova SC", 12), text="Send", bg="black", fg="white", command=Send)
 send_button.pack()
